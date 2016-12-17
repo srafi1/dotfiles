56305f50d89ad479a1ab5596d00242b91e366a06
srafi2 <shakilrafi0@gmail.com>
2016-12-17 15:07:43 -0500

    Commiting with gpush

---
+++ .emacs.d/elpa/foggy-night-theme-20160209.708/foggy-night-theme-autoloads.el
@@ -0,0 +1,24 @@
+;;; foggy-night-theme-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
+
+;;;### (autoloads nil "foggy-night-theme" "foggy-night-theme.el"
+;;;;;;  (22613 39202 539173 73000))
+;;; Generated autoloads from foggy-night-theme.el
+
+(when load-file-name (add-to-list 'custom-theme-load-path (file-name-as-directory (file-name-directory load-file-name))))
+
+;;;***
+
+;;;### (autoloads nil nil ("foggy-night-theme-pkg.el") (22613 39202
+;;;;;;  550466 442000))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; foggy-night-theme-autoloads.el ends here
---
+++ .emacs.d/elpa/foggy-night-theme-20160209.708/foggy-night-theme-pkg.el
@@ -0,0 +1,1 @@
+(define-package "foggy-night-theme" "20160209.708" "Dark low contrast theme with soft and muted colors." '((emacs "24")))

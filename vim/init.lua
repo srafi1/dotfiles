9276a264748ea5274bf3bf7547e30a367e0ee49c
Shakil Rafi <19642235+srafi1@users.noreply.github.com>
2021-08-19 15:01:45 -0400

    more config + autogroups

--- vim/init.lua
+++ vim/init.lua
@@ -77,3 +77,60 @@ vim.o.signcolumn = 'yes'
 vim.o.shell = '/usr/bin/zsh'
 vim.o.shortmess = vim.o.shortmess .. 'c'
 vim.o.wildignore =  vim.o.wildignore .. '.git,node_modules'
+
+-- TODO: have fzf use fd if not switching to telescope
+vim.g.fzf_branch_actions = {
+  delete = {
+    execute = 'G branch -d {branch}'
+  },
+  checkout = {
+    execute = 'G checkout {branch}'
+  }
+}
+
+-- terminal autocmd with fzf
+vim.cmd [[
+  au TermOpen * tnoremap <buffer> <Esc><Esc> <C-\><C-n>
+  au FileType fzf tunmap <buffer> <Esc><Esc>
+  au FileType fzf tnoremap <buffer> <Esc> <C-c>
+]]
+
+-- don't ignore strings and comments with EasyAlign
+vim.g.easy_align_ignore_groups = {}
+
+-- if the directory for a new file doesn't exist, create it
+vim.cmd [[
+  augroup Mkdir
+    autocmd!
+    autocmd BufWritePre *
+      \ if !isdirectory(expand("<afile>:p:h")) |
+      \ call mkdir(expand("<afile>:p:h"), "p") |
+      \ endif
+  augroup END
+]]
+
+-- compile pdf after saving tex file
+vim.cmd [[autocmd BufWritePost *.tex !pdflatex %]]
+vim.g.tex_flavor = 'latex'
+
+-- custom tab config by file extension
+vim.cmd [[
+  autocmd BufRead,BufNewFile 
+    \ *.html,*.css,*.js,*.jsx,*.ts,*.tsx,*.dart,*.lua
+    \ setlocal tabstop=2 shiftwidth=2
+  autocmd BufRead,BufNewFile *.go setlocal noexpandtab
+]]
+
+
+-- break lines and enable spellcheck for document based files
+vim.cmd [[
+  autocmd BufRead,BufNewFile *.tex,*.md,*.mdx,*.wiki,*.txt call WritingMode()
+  function! WritingMode()
+    setlocal linebreak spell
+    nnoremap <buffer> j gj
+    nnoremap <buffer> k gk
+  endfunction
+]]
+
+-- Vimspector function key keybindings
+vim.g.vimspector_enable_mappings = 'HUMAN'

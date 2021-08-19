2dd3d2e7b8ec1ffd7040c82106dfc10f3f0942cd
Shakil Rafi <19642235+srafi1@users.noreply.github.com>
2021-08-19 14:46:02 -0400

    global options

--- vim/init.lua
+++ vim/init.lua
@@ -45,3 +45,35 @@ require'packer'.startup(function()
   use 'ray-x/lsp_signature.nvim'
   use 'MunifTanjim/nui.nvim'
 end)
+
+local opts = {
+  'termguicolors',
+  'relativenumber',
+  'hidden',
+  'wrap',
+  'cursorline',
+  'showmatch',
+  'splitbelow',
+  'splitright',
+  'expandtab',
+  'ignorecase',
+  'smartcase'
+}
+for _, opt in ipairs(opts) do
+  vim.o[opt] = true
+end
+
+vim.g.tokyonight_style = 'night'
+vim.g.tokyonight_colors = {border = '#a9b1d6', comment = '#848cb4'}
+vim.cmd [[colorscheme tokyonight]]
+vim.g.mapleader = ' '
+vim.o.history = 100
+vim.o.tabstop = 4
+vim.o.shiftwidth = 4
+vim.o.mouse = 'nv'
+vim.o.scrolloff = 3
+vim.o.updatetime = 300
+vim.o.signcolumn = 'yes'
+vim.o.shell = '/usr/bin/zsh'
+vim.o.shortmess = vim.o.shortmess .. 'c'
+vim.o.wildignore =  vim.o.wildignore .. '.git,node_modules'

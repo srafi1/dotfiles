1eeee88aeb564e071afb3c91e4fc643565f02a57
Shakil <srafi1@gengar.local>
2026-07-17 21:24:24 -0400

    upgrade lspconfig

--- nvim/lua/config/lsp.lua
+++ nvim/lua/config/lsp.lua
@@ -1,44 +1,49 @@
-local nvim_lsp = require('lspconfig')
+-- shared on-attach behavior (keymaps, navic, signature help)
+vim.api.nvim_create_autocmd('LspAttach', {
+  callback = function(ev)
+    local bufnr = ev.buf
+    local client = vim.lsp.get_client_by_id(ev.data.client_id)
 
-local on_attach = function(client, bufnr)
-  require'lsp_signature'.on_attach{
-    hint_enable = false,
-    hi_parameter = 'IncSearch',
-  }
-  require'nvim-navic'.attach(client, bufnr)
-
-  vim.api.nvim_buf_set_option(bufnr, 'omnifunc', 'v:lua.vim.lsp.omnifunc')
-
-  vim.api.nvim_create_autocmd('CursorMoved', {
-    buffer = bufnr,
-    callback = function()
-      vim.lsp.buf.clear_references()
-      vim.lsp.buf.document_highlight()
+    require'lsp_signature'.on_attach{
+      hint_enable = false,
+      hi_parameter = 'IncSearch',
+    }
+    if client then
+      require'nvim-navic'.attach(client, bufnr)
     end
-  })
 
-  local opts = { buffer = true, silent = true }
-  vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
-  vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, opts)
-  vim.keymap.set('n', 'gm', vim.lsp.buf.implementation, opts)
-  vim.keymap.set('n', 'gr', vim.lsp.buf.references, opts)
-  vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
-  -- vimp.inoremap(opts, '<C-p>', vim.lsp.buf.signature_help)
-  vim.keymap.set('n', 'gy', vim.lsp.buf.type_definition, opts)
-  vim.keymap.set('n', '<space>cw', vim.lsp.buf.rename, opts)
-  vim.keymap.set('n', '<space>ca', vim.lsp.buf.code_action, opts)
-  vim.keymap.set('v', '<space>ca', vim.lsp.buf.code_action, opts)
-  vim.keymap.set('n', '<space>e', function()
-    vim.diagnostic.open_float({border = "single"})
-  end, opts)
-  vim.keymap.set('n', '[e', function()
-    vim.diagnostic.goto_prev({enable_popup = false})
-  end, opts)
-  vim.keymap.set('n', ']e', function()
-    vim.diagnostic.goto_next({enable_popup = false})
-  end, opts)
-  vim.keymap.set('n', '<space>f', vim.lsp.buf.format, opts)
-end
+    vim.bo[bufnr].omnifunc = 'v:lua.vim.lsp.omnifunc'
+
+    vim.api.nvim_create_autocmd('CursorMoved', {
+      buffer = bufnr,
+      callback = function()
+        vim.lsp.buf.clear_references()
+        vim.lsp.buf.document_highlight()
+      end
+    })
+
+    local opts = { buffer = bufnr, silent = true }
+    vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
+    vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, opts)
+    vim.keymap.set('n', 'gm', vim.lsp.buf.implementation, opts)
+    vim.keymap.set('n', 'gr', vim.lsp.buf.references, opts)
+    vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
+    vim.keymap.set('n', 'gy', vim.lsp.buf.type_definition, opts)
+    vim.keymap.set('n', '<space>cw', vim.lsp.buf.rename, opts)
+    vim.keymap.set('n', '<space>ca', vim.lsp.buf.code_action, opts)
+    vim.keymap.set('v', '<space>ca', vim.lsp.buf.code_action, opts)
+    vim.keymap.set('n', '<space>e', function()
+      vim.diagnostic.open_float({border = "single"})
+    end, opts)
+    vim.keymap.set('n', '[e', function()
+      vim.diagnostic.jump({count = -1, float = false})
+    end, opts)
+    vim.keymap.set('n', ']e', function()
+      vim.diagnostic.jump({count = 1, float = false})
+    end, opts)
+    vim.keymap.set('n', '<space>f', vim.lsp.buf.format, opts)
+  end,
+})
 
 -- add borders to hover and limit width
 local orig_util_open_floating_preview = vim.lsp.util.open_floating_preview
@@ -50,18 +55,18 @@ function vim.lsp.util.open_floating_preview(contents, syntax, opts, ...)
   return orig_util_open_floating_preview(contents, syntax, opts, ...)
 end
 
--- diagnostics
-vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(
-  vim.lsp.diagnostic.on_publish_diagnostics, {
-    virtual_text = false,
-  }
-)
-
--- diagnostics sign/highlight definitions
-vim.fn.sign_define('LspDiagnosticsSignWarning',     { text='>>' })
-vim.fn.sign_define('LspDiagnosticsSignError',       { text='>>' })
-vim.fn.sign_define('LspDiagnosticsSignInformation', { text='?'  })
-vim.fn.sign_define('LspDiagnosticsSignHint',        { text='~'  })
+-- diagnostics display + signs
+vim.diagnostic.config({
+  virtual_text = false,
+  signs = {
+    text = {
+      [vim.diagnostic.severity.ERROR] = '>>',
+      [vim.diagnostic.severity.WARN]  = '>>',
+      [vim.diagnostic.severity.INFO]  = '?',
+      [vim.diagnostic.severity.HINT]  = '~',
+    },
+  },
+})
 
 -- individual server config
 -- remember to add installation instructions for new servers to ./lsp-install.sh
@@ -101,7 +106,12 @@ local servers = {
   rust_analyzer = {},
   dartls = {},
 }
-for lsp, config in pairs(servers) do
-  config.on_attach = on_attach
-  nvim_lsp[lsp].setup(config)
+
+-- add cmp capabilities to every server
+local capabilities = require('cmp_nvim_lsp').default_capabilities()
+
+for name, config in pairs(servers) do
+  config.capabilities = capabilities
+  vim.lsp.config(name, config)  -- merges over nvim-lspconfig's shipped defaults
 end
+vim.lsp.enable(vim.tbl_keys(servers))
